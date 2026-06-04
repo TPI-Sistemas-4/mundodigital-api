2336b90d53b4de94550f333eed30fc9c36781bee
LeoLuiten <leonardoluiten@gmail.com>
2026-06-05 01:04:25 +0200

    Fixed deployment error for lacking prisma service inclussion en el modulo de cacrrito

--- src/carrito/carrito.module.ts
+++ src/carrito/carrito.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarritoService } from './carrito.service';
 import { CarritoController } from './carrito.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   providers: [CarritoService],
-  controllers: [CarritoController]
+  controllers: [CarritoController, PrismaService],
 })
 export class CarritoModule {}
